1fd81496c5a0925de7901084ba56a35aac1337f3
Yavuz Yalçıntaş <myavuzyalcintas@gmail.com>
2022-09-07 21:21:24 +0300

    blob

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,50 @@
-import { useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import SugarRate from "./components/SugarRate";
+
+// @ts-ignore
+function coonvertBlob(blob, filename) {
+  const objectUrl = URL.createObjectURL(blob);
+
+  const link = document.createElement("a");
+  link.href = objectUrl;
+  //link.download = filename;
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+
+  setTimeout(() => URL.revokeObjectURL(objectUrl), 5000);
+}
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <SugarRate sugarRate={10} badgeTitle={"TEST"} rank="A" />;
+  const svgRef = useRef<any>();
+
+  useEffect(() => {
+    // @ts-ignore
+    const svg = svgRef.current.innerHTML;
+    const blob = new Blob([svg], { type: "image/svg+xml" });
+    coonvertBlob(blob, `myimage.svg`);
+  }, []);
+
+  const downloadSVG = useCallback(() => {
+    // @ts-ignore
+    const svg = svgRef.current.innerHTML;
+    const blob = new Blob([svg], { type: "image/svg+xml" });
+    coonvertBlob(blob, `myimage.svg`);
+  }, []);
+
+  return (
+    <>
+      <div ref={svgRef}>
+        <SugarRate sugarRate={10} badgeTitle={"TEST"} rank="A" />
+      </div>
+
+      <div>
+        <button onClick={downloadSVG}>Download</button>
+      </div>
+    </>
+  );
 }
 
 export default App;
